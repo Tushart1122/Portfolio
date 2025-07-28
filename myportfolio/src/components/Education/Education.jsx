4ea5e22894c9dbdfc4b7542636acd12c5fe438dc
Tushar <2022bcs058@sggs.ac.in>
2025-07-29 00:30:26 +0530

    FIrst Commit in Education

--- myportfolio/src/components/Education/Education.jsx
+++ myportfolio/src/components/Education/Education.jsx
@@ -5,7 +5,7 @@ const Education = () => {
   return (
     <section
       id="education"
-      className="py-1 pb-24 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-3"
+      className="py-0 pb-20 px-[12vw] md:px-[7vw] lg:px-[16vw] font-sans bg-skills-gradient clip-path-custom-3"
     >
       {/* Section Title */}
       <div className="text-center mb-16 ">
